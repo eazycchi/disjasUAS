932ee72623d599c75cd9bd78db51a04253c38dcd
eazycchi <mochizza123@gmail.com>
2021-12-21 23:30:50 +0700

    check point-1

--- js/index.js
+++ js/index.js
@@ -91,22 +91,7 @@ class Flightradar24Data {
 }
 
 async function renderData() {
-  //     try{
-  //             const response = await fetch('https://flask-flight-radar-apps.azurewebsites.net/WIOO/details/1');
-  //             const responseJson = await response.json();
-  //             const airport_name = document.getElementById('airport-name');
-  //             airport_name.innerText = responseJson.name;
-  //             const airport_code = document.getElementById('airport-code');
-  //             airport_code.innerText = responseJson.code.iata;
-  //             console.log(responseJson);
-  // } catch(err){
-  //         console.log(err);
-  //     }
-
-  //memasukkan nama airport menggunakan data response
-  const data_details = await Flightradar24Data.getAirportDetails({
-    iata: "WIOO",
-  });
+  const data_details = await Flightradar24Data.getAirportDetails();
   const airport_name = document.getElementById("airport-name");
   airport_name.innerText = data_details.name;
 }
